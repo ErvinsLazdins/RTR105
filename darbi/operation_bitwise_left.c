3c5a07cce09dbee144bac321651ceb2b8df769c9
user <lazdinservins@gmail.com>
2019-10-03 16:11:32 +0300

    20191003_16_11

---
+++ darbi/operation_bitwise_left.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/*
+
+void main()
+{
+
+char a=10; // atmina paradisies mainigais a (izmers 1byte, saturs 0000 1010)
+printf ("Operācija <<: %d<<1 rezultāts %d\n",a,a<<1);// sagaidam skaitli 20
+
+char b;// atmiņā paradīsies mainīgas b (izmērs  byte, saturs)
+printf("B mainīgā \"sākuma\" stāvoklis (uzreiz pēc deklarēšanas): %d\n",b);
+
+b=a<<2;
+printf("Operācija <<: %d<<%n rezultāts %d\n",a,b); // sagaidam skaitli 40
+
+b=a<<2; // šeit ir divas operācijas -=(piešķiršana) un <<(bīdīšana pa bitiem pa kreisi)
+printf("Operācija <<: %d<<d rezultāts %d\n",a,2,b);// saigadam skaitli 40
+
+char c;
+printf("Papetisim << operaciju\n----------------n");
+printf("Cienijamais lietotaj, ludzu, ievadi operacijas 1. operandu: ");
+scanf("%d",&a);
+printf("Cienijamais lietotaj, ludzu, ievadi operacijas 2. operandu: ");
+scanf("%d",&c);
+b=a<<c;
+printf("--------------------\nSkaties, kas ir sanacis: %d\n":,b)
+}
+
+*/
