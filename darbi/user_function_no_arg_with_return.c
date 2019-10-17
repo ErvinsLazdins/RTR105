74db66ff463348b06723619979f2bf7930ff692c
user <lazdinservins@gmail.com>
2019-10-17 15:54:59 +0300

    20191017_15_55

---
+++ darbi/user_function_no_arg_with_return.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int user_function_5()
+{
+int i_uf;
+printf("Cien. lietotāj, lūdzu, ievadi vienu skatli (ar  5 jautājumi beigsies): ");
+scanf("%d",&i_uf);
+return i_uf;
+}
+
+
+void main()
+{
+int i_main, N=5;
+
+i_main=user_function_5();
+
+while(i_main!=N)
+{
+printf("Reaģējot uz manas lietotāja funkcijas darbībām, tu esi ievadījis %d\n",i_main);
+i_main=user_function_5();
+ }
+printf("Tā kā esi ievadījis 5, tad jautājumu vairs nav!\n");
+}
